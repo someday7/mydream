f3dc918a0b1d2e3aca5f038c087d12733ebe7804
someday7 <liuwenhu7@gmail.com>
2014-10-27 17:06:03 +0800

    新建版本
    Signed-off-by: someday7 <liuwenhu7@gmail.com>

---
+++ caches/error_log.php
@@ -0,0 +1,62 @@
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1423
+<?php exit;?>10-16 10:05:06 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1424
+<?php exit;?>10-16 10:05:06 | 2 | mysql_connect(): Access denied for user ''@'localhost' (using password: NO) | phpcms\libs\classes\mysql.class.php | 56
+<?php exit;?>10-16 10:05:06 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
+<?php exit;?>10-16 10:05:06 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>10-16 10:05:06 | 2 | mysql_errno() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 392
+<?php exit;?>10-16 10:05:06 | 2 | mysql_error() expects parameter 1 to be resource, boolean given | phpcms\libs\classes\mysql.class.php | 388
+<?php exit;?>10-16 10:12:41 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:12:46 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:12:56 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:13:13 | 2048 | Only variables should be passed by reference | phpcms\modules\member\index.php | 231
+<?php exit;?>10-16 10:14:02 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:09 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:11 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:11 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:12 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:13 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:16 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:17 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:19 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:23 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:37 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:41 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:42 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:43 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:44 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:44 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:45 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:46 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:18:57 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:19:07 | 2048 | Only variables should be passed by reference | phpcms\modules\member\index.php | 231
+<?php exit;?>10-16 10:24:53 | 2 | extract() expects parameter 1 to be array, string given | phpcms\modules\poster\space.php | 165
+<?php exit;?>10-16 10:29:48 | 2 | Illegal string offset 'enablehits' | phpcms\modules\poster\index.php | 104
+<?php exit;?>10-16 10:33:06 | 2 | mkdir(): File exists           | phpcms\modules\admin\googlesitemap.php | 175
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://php.local/dream/',
+    'authkey' => 'WqHe5FkGdBNUWIlafEcYwK8OlwanTNzN',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
